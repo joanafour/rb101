05b0c6a4cc77a788c4a45ffe1f57926eef752c81
joanafour <j.99.nika@gmail.com>
2023-08-03 10:22:53 -0600

    8.2

--- lesson_2/rps2.rb
+++ lesson_2/rps2.rb
@@ -5,15 +5,20 @@ def prompt(message)
 end
 
 def display_results(player, computer)
-  if (choice == 'rock' && computer_choice == 'scissors') ||
-    (choice == 'paper' && computer_choice == 'rock') ||
-    (choice == 'scissors' && computer_choice == 'paper')
+  if (player == 'rock' && computer == 'scissors') ||
+  (player == 'paper' && computer == 'rock') ||
+  (player == 'scissors' && computer == 'paper')
   prompt("You won!")
-elsif (choice == 'rock' && computer_choice == 'paper') ||
-      (choice == 'paper' && computer_choice == 'scissors') ||
-      (choice == 'scissors' && computer_choice == 'rock')
+elsif (player == 'rock' && computer == 'paper') ||
+  (player == 'paper' && computer == 'scissors') ||
+  (player == 'scissors' && computer == 'rock')
   prompt("Computer won!")
-  
+else
+  prompt("It's a tie!")
+end
+end
+
+
 loop do
   choice = ''
   loop do
@@ -26,6 +31,7 @@ loop do
     prompt("That's not a valid choice.")
     end
   end
+
 
   computer_choice = VALID_CHOICES.sample
   Kernel.puts("You chose : #{choice}. Computer chose: #{computer_choice}")
