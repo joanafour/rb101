0d33ba295ffc90974d19792891e84b638e019471
joanafour <j.99.nika@gmail.com>
2023-08-15 14:18:32 -0600

    8.15

--- lesson_3/easy1_1.rb
+++ lesson_3/easy1_1.rb
@@ -3,18 +3,48 @@ numbers.uniq
 #non-mutating, returns number elements that are not duplicates [1, 2, 2, 3]
 puts numbers
 #outputs 1 2 2 3
-
+#2
+=begin
+!= is not equal to, could use in conditionals
+!(object) not the object, returns false
+! ususally indicates method is mutating (! and ? are actually part of the method name not just the syntax)
+? error
+!!  is just =?
+=end
 #3
 advice = "Few things in life are as important as house training your pet dinosaur."
 advice.sub!("important", "urgents")
 
 #4
 numbers = [1, 2, 3, 4, 5]
-numbers.delete_at(1) # deletes the index; numbers calls on the delete method and passes in the array delete_at methods iterates through the array
-# they operate on the contents of the referenced array and modify it in place, rather than juist returning a modified version of the array
+numbers.delete_at(1) # deletes the index; arrays object calls on the delete method and passes in the array delete_at methods iterates through the array
+# they operate on the contents of the referenced array and modify it in place, rather than just returning a modified version of the array
 
 numbers.delete(1) #deletes the number 1
 
+#5
+r = 10..100
+r.include?(42)
+
+#6
 famous_words = "seven years ago..."
 famous_words >> "four score and "
-famous_words = "Fours c"
+puts "Four score and #{famous_words}."
+
+#7
+flintstones = ["Fred", "Wilma"]
+flintstones << ["Barney", "Betty"]
+flintstones << ["BamBam", "Pebbles"]
+
+flintstones.flatten!
+
+#8
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+a = []
+flintstones.each_key{|key| a << key}
+b []
+flintstones.each_value{|value| b << value}
+
+
+c = flintstones.slice("Barney")
+c.to_a
