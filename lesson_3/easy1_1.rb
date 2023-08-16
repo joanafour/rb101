99ec4cb4ab09787c802e9bdade9616b5754ae23f
joanafour <j.99.nika@gmail.com>
2023-08-16 12:43:07 -0600

    8.16

--- lesson_3/easy1_1.rb
+++ lesson_3/easy1_1.rb
@@ -6,10 +6,10 @@ puts numbers
 #2
 =begin
 != is not equal to, could use in conditionals
-!(object) not the object, returns false
-! ususally indicates method is mutating (! and ? are actually part of the method name not just the syntax)
+!(object) turns an object into the opposite of their boolean equivalent
+! ususally indicates method is mutating (! and ? are actually part of the method name not just the synax)
 ? error
-!!  is just =?
+!!(object) turns object into its boolean equivalent
 =end
 #3
 advice = "Few things in life are as important as house training your pet dinosaur."
@@ -48,3 +48,65 @@ flintstones.each_value{|value| b << value}
 
 c = flintstones.slice("Barney")
 c.to_a
+
+#1
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+ages.include?("Spot")
+# Methods has_key?, key?, and member? are aliases for #include?.
+
+#2
+munsters_description = "The Munsters are creepy in a good way."
+munsters_description.capitalize!
+munsters_description.upcase
+munsters_description.downcase
+munsters_description.swapcase
+
+Upcases each downcase character and downcases each upcase character; returns self if any changes, nil otherwise.
+
+#3
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
+additional_ages = { "Marilyn" => 22, "Spot" => 237 }
+
+ages.merge!(additional_ages)
+
+#4
+advice = "Few things in life are as important as house training your pet dinosaur."
+advice =~ /dino/
+advice.include?("dino")
+advice.match?
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+#5
+flintstones = %w(Fred barney wilma betty bambam pebbles)
+#6
+flintstones.push("DIno")
+#7
+advice = "Few things in life are as important as house training your pet dinosaur."
+advice.slice!("as house training ")
+advice.slice(0, advice.index('house'))
+ # [start, length]
+# the string slice! mutates the string and returns as house training
+# the string slice only returns the sliced out part and does not affect the original string
+#8
+statement = "The Flintstones Rock!"
+
+statement.count 't'
+
+title = "Flintstone Family Members"
+title.center(40)
+
+y = "The Flintstones Rock!"
+
+x = 1
+while x < 10
+  puts y
+  y.prepend " "
+  x += 1
+end
+1
+#2.
+
+puts "the value of 40 + 2 is " + (40 + 2)
+# trying to use string concatenation with an integer
+# could use string interpolation instead "the value of 40 + 2 is #{40 + 2}."
+# the 
