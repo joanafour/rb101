d70c6ca271877c1a259c0ec80d2cd9c18a39677b
joanafour <j.99.nika@gmail.com>
2023-08-19 23:58:34 -0600

    8.19

---
+++ lesson_3/again.rb
@@ -0,0 +1,16 @@
+
+def factors(number)
+  divisor = number
+  factors = []
+  until divisor == 0
+    factors << number / divisor if number % == 0
+    divisor -= 1
+  elsif divisor <- 0
+    puts "That is not valid input."
+    break
+  end
+  end
+end
+
+
+#4
--- lesson_3/easy1_1.rb
+++ lesson_3/easy1_1.rb
@@ -123,3 +123,16 @@ def factors(number)
   factors
 end
 
+def factors(number)
+  divisor = number
+  factors = []
+  until divisor == 0
+    factors << number / divisor if number % == 0
+    divisor -= 1
+  elsif divisor <- 0
+    puts "That is not valid input."
+    break
+  end
+  end
+end
+
