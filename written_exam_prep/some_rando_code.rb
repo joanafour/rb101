de3df21aa824742ce4c7c2b4d099892adf47040e
joanafour <j.99.nika@gmail.com>
2023-08-24 23:48:29 -0600

    8.24

--- written_exam_prep/some_rando_code.rb
+++ written_exam_prep/some_rando_code.rb
@@ -1,3 +1,4 @@
+=begin
 x = [1, 2, 3, 4, 5]
 
 for i in x.reverse do
@@ -7,3 +8,19 @@ end
 
 puts "Done!"
 puts a
+
+
+# can access a
+=end
+
+
+
+x = 2
+while x <= 10 do
+   x += 1
+   j = "joana"
+end
+
+
+puts x
+puts j
