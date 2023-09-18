7b7c6396cdd4c6b7273ca3b0188b2f84ecc253c6
joanafour <j.99.nika@gmail.com>
2023-09-17 21:08:40 -0600

    9.17

--- interview_prep/code_wars/4.rb
+++ interview_prep/code_wars/4.rb
@@ -1,5 +1,8 @@
 alphabet = ("a".."z").to_a
 
+
+
+
 # input is an array of strings (can be lower or uppercase; all characters, no spaces)
 # But more implicity some
 # the characters in the string
@@ -7,16 +10,27 @@ alphabet = ("a".."z").to_a
 # the alphabet
 # the position of characters in the alphabet
 
-#  have to access the characters in each string to match to positions in the alphabet
-#  have to access the alphabet and places of letters
-# have to process each character in the string and match it up to hwat place it holds in the word
-# compare that to w
 
+# MENTAL MODEL
+# have to have access to the alphabet characters and their respective positions in the alphabet
+# have to access the to the characters in each string and identify their position in the string
+# check to see if the positions held by the character in the string is the same as the position
+# it holds in the alphabet
 
 # output is an array of integers, with each integer representing the total number of times
 # the characters in each string occupy the same position as that in the alphabet
-#
+
+
+# DATA STRUCTURE
+# data structure for alphabet; a array where  values are set equal to range [a..z] & [1..26]
+# data structure for string - we are given an array of multiple strings
+# break up the given array into individual strings and those strings into characters that
+# return/have the value of its position
+
+=begin
+ALGORITHM
 
 
 
 
+=end
---
+++ interview_prep/code_wars/PEDAC.rb
@@ -0,0 +1,43 @@
+
+=begin
+
+Understand the problem:
+1. Digest the problem. Don't rush. Read carefully. Our brain fills in gaps if we don't do it explicity.
+
+2. Write out the Inputs and Outputs including the implicit ones; write out implicit concepts as well
+**Problem domain is the area of expertise or application that needs to be examined to solve a problem.
+It limits the scope of the problem.**
+
+3. Write out the rules
+What are the rules or the constraints that we need to consider?
+Write out defaults (if there are any).
+4. Write out claifactory questions?
+
+Mental model:
+A mental model is an explanation of someone's thought process about how something
+works in the real world.
+Divide it into parts.
+
+Examples/Test Cases:
+1. To validate our problem check on what is the expected output
+If I follow this rule what is the output?
+
+Data Structure:
+This is the data structure that we will work with to convert the input to output
+How we represent data that we will work with when converting the input to output: classes, variables, strings, integers
+methods, different datatypes
+DSata structure and algorithm go hand in hand
+
+Algorithm:
+Coming up with a series of instructions that will take us from the input to the output
+Lose  flexibility if written in programming language construct (i.e., use a for loop, use each, etc)
+Change has to be done to algorithm if there are changes realized during implementation
+
+One promblem -> multipe agorithms
+
+Where to start?
+If you've got a mental model you can start from there; otherwise you can start with the data structure
+
+
+
+=end
